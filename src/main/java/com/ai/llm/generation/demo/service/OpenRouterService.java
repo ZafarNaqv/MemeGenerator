b8db2115bcaca35cb067b7c68dcd3a623dfa8362
Zafar Naqvi <xafar.nasim@gmail.com>
2025-08-05 11:56:43 +0200

    Added aspects to restrict endpoints

--- src/main/java/com/ai/llm/generation/demo/service/OpenRouterService.java
+++ src/main/java/com/ai/llm/generation/demo/service/OpenRouterService.java
@@ -6,6 +6,7 @@ import com.ai.llm.generation.demo.model.dto.ChatRequestDTO;
 import com.ai.llm.generation.demo.model.dto.ChatResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import retrofit2.Response;
 
@@ -23,8 +24,12 @@ public class OpenRouterService {
         this.api = api;
     }
     
-    public String getCompletion(String name) {
+    public String getCompletion(String name, OAuth2User principal) {
         String prompt = "Give 5 interesting facts in small bullet points directly about the name: " +name;
+        if(principal != null) {
+            String email = principal.getAttribute("email");
+            logger.info("User {} searched for name {}", email, name);
+        }
         ChatRequestDTO request = new ChatRequestDTO(
                 LLM_MODEL,
                 List.of(new MessageDTO("user", prompt))
@@ -43,7 +48,7 @@ public class OpenRouterService {
                 return "Error: " + response.code() + " - " + (response.message() != null ? response.message(): "Unknown error");
             }
         } catch (IOException e) {
-            logger.error("IOException recieved: " + e.getMessage());
+            logger.error("IOException received: {}", e.getMessage());
             return "Exception occurred: " + e.getMessage();
         }
     }
